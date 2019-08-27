abcbf72ca1a62a359fc5c719dbb9bd9b16b44cb2
Ville Lehtovirta <villel_92@hotmail.com>
2019-08-27 18:47:55 +0300

    Modify sidebar for new content + add new Field(s) for attributes & stats

--- src/Webgame.js
+++ src/Webgame.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './Webgame.css';
 
-class Square extends React.Component {
+class Field extends React.Component {
   render() {
     return (
       <ul>
@@ -16,35 +16,42 @@ class Webgame extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: "default",
+      spellpower: 0,
+      intellect: 0,
+      spirit: 0,
+      mp5: 0,
+      spellCrit: 0,
+      hps: 0,
+      hpm: 0,
+      healOom: 0,
     };
   }
 
   renderText(i) {
-    return <Square value={i} />;
+    return <Field value={i}/>;
   }
 
   render() {
     return (
       <div className="Webgame" >
         <div className="Webgame-leftbody">
-          {this.renderText(this.state.value)}
-          {this.renderText(this.state.value)}
-          {this.renderText(this.state.value)}
-          {this.renderText(this.state.value)}
-          {this.renderText(this.state.value)}
+          {this.renderText(this.state.spellpower)}
+          {this.renderText(this.state.intellect)}
+          {this.renderText(this.state.spirit)}
+          {this.renderText(this.state.mp5)}
+          {this.renderText(this.state.spellCrit)}
         </div>
         <div className="Webgame-rightbody">
-          {this.renderText(this.state.value)}
-          {this.renderText(this.state.value)}
-          {this.renderText(this.state.value)}
-          {this.renderText(this.state.value)}
-          {this.renderText(this.state.value)}
+          {this.renderText(this.state.hps)}
+          {this.renderText(this.state.hpm)}
+          {this.renderText(this.state.healOom)}
         </div>
         <div className="Webgame-sidebar">
-          <button className="myButton">Fire</button>
-          <button className="myButton">Frost</button>
-          <button className="myButton">Arcane</button>
+          <button className="myButton">Holy Paladin</button>
+          <button className="myButton">Holy Priest</button>
+          <button className="myButton">Discipline Priest</button>
+          <button className="myButton">Resto Shaman</button>
+          <button className="myButton">Resto Druid</button>
         </div>
       </div>
     );
